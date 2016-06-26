7a32ac8965304de926140f692f0e541260b81205
William <William@192.168.1.10>
2016-06-26 12:59:39 +0200

    First implement for race

---
+++ src/Human.java
@@ -0,0 +1,10 @@
+
+public class Human extends Race {
+	private static final int HumanHealth = 7;
+	private static final int HumanAttack = 4;
+	
+	public Human(){
+		super(HumanHealth,HumanAttack);
+	}
+
+}
--- src/Race.java
+++ src/Race.java
@@ -1,5 +1,21 @@
 
 public abstract class Race {
 	 private int health;
+	 private int attack;
+	 
+	 public Race(int health,int attack){
+		 this.health=health;
+		 this.attack=attack;
+	 }
+	 
+
+	public int getAttack() {
+		return attack;
+	}
+
+
+	public int getHealth() {
+		return health;
+	}
 
 }
